f9fa6f473ecf71e5befa74dd54e0a0daeb84c974
SebastianFischer164158 <s164158@student.dtu.dk>
2019-03-27 17:33:56 +0100

    Update Plotfile.py

--- RSA/Plots/Plotfile.py
+++ RSA/Plots/Plotfile.py
@@ -1,13 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.ticker import MaxNLocator
 #import seaborn as sns
 plt.style.use("seaborn-darkgrid")
-
-#fig, ax = plt.subplots(1,figsize=(12,9))
-
-#@@@@@@@@@@@@@@@@@@@@ DATA @@@@@@@@@@@@@@@@@@@@#
 ESP32xtick = np.arange(40,621,20) #number of decimals
 ESP32ytick = np.arange(0,330000,25000)
+#@@@@@@@@@@@@@@@@@@@@ DATA @@@@@@@@@@@@@@@@@@@@#
 keysizes_ESP32 = np.array([40,60,80,100,120,
                           140,155,160,180,200,
                           220,240,260,280,300,
@@ -16,12 +14,27 @@ keysizes_ESP32 = np.array([40,60,80,100,120,
                           500,520,540,560,580,
                           600,617,620]) #155 is 512RSA, 309 is RSA 1024, 617 is 2048 RSA
 
+keygen_time_ESP32 = np.array([2,3,3,4,4,
+                              4,4,6,6,6,5,
+                              6,6,6,7,8,10,
+                              11,12,12,12,13,
+                              13,13,13,15,15,
+                              15,15,15,16,17, 17]) #unit in ms
+
+keygen_time_UNO = np.array([21])
+keygen_time_ESP8266 = np.array([3, 4, 5])
+keygen_time_MEGA = np.array([21, 28, 40, 49, 60, 69, 74, 86, 97, 104, 114])
+
+
 encrypt_times_ESP32 = np.array([11, 19, 34, 47, 57,
                                 67, 74, 101, 122, 135, 152,
                                 164, 180, 199, 214, 223, 304,
                                 348, 361, 390, 401, 431,
                                 448, 480, 512, 529, 550,
                                 575, 596, 612, 635, 651, 657]) #unit in ms
+encrypt_times_UNO = np.array([427])
+encrypt_times_ESP8266 = np.array([39, 77, 124])
+encrypt_times_MEGA = np.array([432, 877, 1374, 2004, 2692, 3390, 4150, 4451, 5462, 6375, 7537])
 
 decryption_times_ESP32 = np.array([160, 447, 1188, 2254, 3287,
                                    4717, 5841, 8523, 12153, 14976, 18679,
@@ -30,12 +43,13 @@ decryption_times_ESP32 = np.array([160, 447, 1188, 2254, 3287,
                                    131548, 148389, 164413, 178871, 197647,
                                    214849, 235389, 251535, 276178, 290122, 294126]) #unit in ms
 
-keygen_time_ESP32 = np.array([2,3,3,4,4,
-                              4,4,6,6,6,5,
-                              6,6,6,7,8,10,
-                              11,12,12,12,13,
-                              13,13,13,15,15,
-                              15,15,15,16,17, 17]) #unit in ms
+decryption_times_UNO = np.array([]) #could not decrypt
+decryption_times_ESP8266 = np.array([427, 1932])
+decryption_times_MEGA = np.array([7159, 22086, 50114, 99094, 161963, 248439, 330782, 380820, 550030, 723120, 958844])
+
+
+
+#@@@@@@@@@@@@@@@@@@@@ PLOTS @@@@@@@@@@@@@@@@@@@@#
 
 
 #plt.plot(encrypt_times_ESP32,keysizes_ESP32)
@@ -43,6 +57,11 @@ keygen_time_ESP32 = np.array([2,3,3,4,4,
 
 #plt.plot(keysizes_ESP32,keygen_time_ESP32)
 
+
+
+#@@@@@@@@@@@@@@@@@@@@ KEY SIZS VS DECRYPTION TIMES @@@@@@@@@@@@@@@@@@@@#
+
+#### RSA_ESP32_KeySize VS decryptionTime ####
 
 fig2, ax2 = plt.subplots(1,figsize=(12,9))
 ax2.plot(keysizes_ESP32,decryption_times_ESP32,'k')
@@ -60,8 +79,35 @@ ax2.set_xlabel('[Public Key Size (# Decimals)]', fontsize='medium')
 plt.xticks(ESP32xtick)
 plt.yticks(ESP32ytick)
 
-y1 = 5841
 plt.plot(155, 5841, 'xr')
 plt.plot(309, 41984, 'xr')
 plt.plot(617, 290122, 'xr')
+
+
+#### RSA_KeySize VS decryptionTimeUNO ####
+#UNO could not handle it.
+
+#### RSA_KeySize VS decryptionTimeESP8266 ####
+ESP8266xtick = np.arange(40,62,2)
+fig3, ax3 = plt.subplots(1,figsize=(12,9))
+ax3.plot(ESP32xtick[0:2],decryption_times_ESP8266,'k') #only two datapoints, talk about this.
+ax3.set_xticks(ESP8266xtick)
+ax3.set_ylabel('[ms]', fontsize=10)
+ax3.set_xlabel('[Public Key Size (# Decimals)]', fontsize='medium')
+
+#### RSA_KeySize VS decryptionTimeMEGA####
+fig4, ax4 = plt.subplots(1,figsize=(12,9))
+ax4.plot(keysizes_ESP32[0:11],decryption_times_MEGA,'k') #only two datapoints, talk about this.
+#ax4.set_xticks(ESP8266xtick)
+ax4.axhline(y=330782, color='gray', linestyle=':') #RSA-155 (RSA-512 bits)
+ax4.text(231,330782, "RSA-512", va='center', ha="left", bbox=dict(facecolor="w",alpha=0.5))
+
+ax4.set_ylabel('[ms]', fontsize=10)
+ax4.set_xlabel('[Public Key Size (# Decimals)]', fontsize='medium')
+ax4.plot(155, 330782, 'xr')
+ax4.text(29,330782, "330782")
+
+#@@@@@@@@@@@@@@@@@@@@ KEY SIZS VS DECRYPTION TIMES @@@@@@@@@@@@@@@@@@@@#
+
+
 plt.show()
